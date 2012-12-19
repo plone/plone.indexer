3fd7ba9e797d3051c77f5581a3ca5a617baa4157
Kai Lautaportti <kai.lautaportti@hexagonit.fi>
2012-12-19 13:55:35 +0200

    Maintain information about the wrapped function in @indexer
    Now the @indexer decorated function has the __doc__, __module__, __name__, etc information from the original wrapped function.
    
    This is particularly useful when generating documentation for code
    using something like Sphinx's autodoc extension. Previously all the
    @indexer decorated functions had the (same) documentation of the DelegatingIndexerFactory class instead of the wrapped functions making the autogenerated documentation rather pointless.

--- plone/indexer/delegate.py
+++ plone/indexer/delegate.py
@@ -1,6 +1,7 @@
 from zope.interface import implements
 from zope.interface.declarations import Implements, implementedBy
 from plone.indexer.interfaces import IIndexer
+from functools import update_wrapper
 
 
 class DelegatingIndexer(object):
@@ -25,6 +26,7 @@ class DelegatingIndexerFactory(object):
     def __init__(self, callable):
         self.callable = callable
         self.__implemented__ = Implements(implementedBy(DelegatingIndexer))
+        update_wrapper(self, callable)
 
     def __call__(self, object, catalog=None):
         return DelegatingIndexer(object, catalog, self.callable)
