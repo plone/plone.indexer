4eb7295d6387b2b548bb8f3a0a87b5cea7db42dc
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:41:17 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -17,33 +17,38 @@ long_description = (
     + '\n'
     )
 
-setup(name='plone.indexer',
-      version=version,
-      description="Hooks to facilitate managing custom index values in "
-                  "Zope 2/CMF applications",
-      long_description=long_description,
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: BSD License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-        ],
-      keywords='plone cmf zope catalog index',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/plone.indexer',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.interface',
-          'zope.component',
-          'Products.CMFCore',
-          'Products.ZCatalog',
-      ],
-      )
+setup(
+    name='plone.indexer',
+    version=version,
+    description="Hooks to facilitate managing custom index values in "
+                "Zope 2/CMF applications",
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone : 4.3",
+        "Framework :: Plone : 5.0",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='plone cmf zope catalog index',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/pypi/plone.indexer',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'Products.CMFCore',
+        'Products.ZCatalog',
+    ],
+    )
