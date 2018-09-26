27369676be02cd3c63f1b5c8a5aca22b86437ad4
Maurits van Rees <maurits@vanrees.org>
2018-09-26 18:27:48 +0200

    Preparing release 1.0.5
    [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import os
 
 
-version = '1.0.5.dev0'
+version = '1.0.5'
 description = 'Hooks to facilitate managing custom index values in Zope 2/CMF applications'  # noqa
 long_description = ('\n\n'.join([
     open('README.rst').read(),
